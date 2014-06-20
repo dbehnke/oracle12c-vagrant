cb47321ea88cddc370dbab4561e9ff2237ce80ba
Dave Behnke <dbehnke74@gmail.com>
2014-06-19 22:17:53 -0400

    modified installpatch to use psu 18031528

--- patches/installpatch.sh
+++ patches/installpatch.sh
@@ -11,8 +11,8 @@ unzip /vagrant/patches/p6880880_121010_Linux-x86-64.zip
 cd ../
 mkdir 2
 cd 2
-unzip /vagrant/patches/p17552800_121010_Linux-x86-64.zip
-cd 17552800
+unzip /vagrant/patches/p18031528_121010_Linux-x86-64.zip
+cd 18031528
 ../../1/OPatch/opatch apply -ocmrf ${HOME}/ocm.rsp
 
 #you should apply datapatch if database was created before patches
