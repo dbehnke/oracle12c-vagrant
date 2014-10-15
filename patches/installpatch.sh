67faf111266c4456b8c01373002d569a78d6c324
Dave Behnke <dbehnke74@gmail.com>
2014-10-14 22:28:44 -0400

    update for patch 19121550

--- patches/installpatch.sh
+++ patches/installpatch.sh
@@ -11,8 +11,8 @@ unzip /vagrant/patches/p6880880_121010_Linux-x86-64.zip
 cd ../
 mkdir 2
 cd 2
-unzip /vagrant/patches/p18031528_121010_Linux-x86-64.zip
-cd 18031528
+unzip /vagrant/patches/p19121550_121010_Linux-x86-64.zip
+cd 19121550
 ../../1/OPatch/opatch apply -ocmrf ${HOME}/ocm.rsp
 
 #you should apply datapatch if database was created before patches
